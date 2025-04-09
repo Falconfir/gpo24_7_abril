562dded22cf43094bd5423cff313035a8b872cfd
Carlos Emilio Lopez del Angel <carlosemiliolda@gmail.com>
2025-04-08 18:40:49 -0600

    se cambio la clase Cuadrado

--- demo.py
+++ demo.py
@@ -1,2 +1,14 @@
-class Cuadro:
-    pass
+class Cuadrado:
+    def __init__(self,lado1,lado2):
+        self.lado1=lado1
+        self.lado2=lado2
+
+    def area (self):
+        return self.lado1 * self.lado2
+
+lado1=int(input("ingresa un lado"))
+lado2=int(input("ingresa otro lado"))
+cuadrado= Cuadrado(lado1, lado2)
+resultado= cuadrado.area()
+
+print(f"el resultado es{resultado}")
